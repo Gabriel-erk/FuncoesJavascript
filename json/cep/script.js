417b59314f98bb3c70c6705bf2bfc0f36581b3fe
Gabriel <gabrielmmve13@gmail.com>
2024-03-12 22:13:07 -0300

    concluindo formulario para exibir cep, com api

--- json/cep/script.js
+++ json/cep/script.js
@@ -1,7 +1,33 @@
 async function buscarCep() {
-  const respostaCep = await fetch("https://viacep.com.br/ws/01001000/json/");
+  /* função que evita que o javascript recarregue a pagina ao clicar no botao, isto acontce por causa do formulario */
+  event.preventDefault();
+  // Obtém o valor do CEP do campo de entrada
+  const cep = document.getElementById('cep').value;
 
-  const dadosCep = await respostaCep.json();
-  const cepUsuario = document.getElementById("cep");
-  
+  // Faz uma solicitação para a API ViaCEP usando o CEP fornecido
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    // Converte a resposta da solicitação para JSON
+    .then(response => response.json())
+    // Manipula os dados retornados pela API ViaCEP
+    .then(data => {
+      // Verifica se não há erro na resposta
+      if (!data.erro) {
+        // Preenche os campos do formulário com os dados do endereço
+        document.getElementById('logradouro').value = data.logradouro;
+        document.getElementById('complemento').value = data.complemento;
+        
+        if(data.complemento == ''){
+          document.getElementById('complemento').value = "nenhum";
+        }
+        document.getElementById('bairro').value = data.bairro;
+        document.getElementById('localidade').value = data.localidade;
+        document.getElementById('UF').value = data.uf;
+        document.getElementById('DDD').value = data.ddd;
+      } else {
+        // Se houver erro, exibe uma mensagem de alerta informando que o CEP não foi encontrado
+        alert('CEP não encontrado');
+      }
+    })
+    // Captura e trata quaisquer erros que possam ocorrer durante a solicitação
+    .catch(error => console.error('Erro ao buscar endereço:', error));
 }
---
+++ json/cep/script2.js
@@ -0,0 +1,22 @@
+async function buscarEndereco() {
+  /* função que evita que o javascript recarregue a pagina ao clicar no botao, isto acontce por causa do formulario */
+  event.preventDefault();
+  const logradouro = document.getElementById('logradouro').value;
+  fetch(`https://viacep.com.br/ws/${logradouro}/json/`)
+   
+  .then(response => response.json())
+  .then(data => {
+    if(!data.erro) {
+      document.getElementById('cep').value = data.cep;
+      document.getElementById('complemento').value = data.complemento;
+      document.getElementById('bairro').value = data.bairro;
+      document.getElementById('localidade').value = data.localidade;
+      document.getElementById('UF').value = data.uf;
+      document.getElementById('DDD').value = data.ddd;
+    }
+    else {
+      alert('Endereço não encontrado')
+    }
+  })
+  .catch(error => console.error('erro ao buscar endereço', error))
+}
